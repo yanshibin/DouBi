2b3e31800464c356981a29dbffce79c3808ef8a0
Kristina <8521118@gmail.com>
2018-11-19 00:20:14 +0800

    Update ssrmu.sh

--- ssrmu.sh
+++ ssrmu.sh
@@ -853,7 +853,7 @@ Debian_apt(){
 # 下载 ShadowsocksR
 Download_SSR(){
 	cd "/usr/local"
-	wget -N --no-check-certificate "https://github.com/ToyoDAdoubi/shadowsocksr/archive/manyuser.zip"
+	wget -N --no-check-certificate "https://raw.githubusercontent.com/yanshibin/DouBi/master/shadowsocksr-manyuser.zip"
 	#git config --global http.sslVerify false
 	#env GIT_SSL_NO_VERIFY=true git clone -b manyuser https://github.com/ToyoDAdoubi/shadowsocksr.git
 	#[[ ! -e ${ssr_folder} ]] && echo -e "${Error} ShadowsocksR服务端 下载失败 !" && exit 1
